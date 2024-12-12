0889caba142a27c3478d46abda8bf9caa67c3a0e
AstroNostra <astronostra2023@gmail.com>
2024-12-12 13:08:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,44 @@
 const cards = [
-    { name: "The_Magician", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Magician.jpg.jpg" },
-    { name: "The_Empress", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Empress.jpg.jpg" },
-    { name: "Death", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/Death.jpg.jpg" },
-    { name: "The_Fool", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Fool.jpg.jpg" },
-    { name: "The_Hanged_Man", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Hanged_Man.jpg.jpg" },
-    { name: "The_Lovers", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Lovers.jpg.jpg" },
-    { name: "The_Star", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Star.jpg.jpg" },
-    { name: "The_Tower", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Tower.jpg.jpg" },
-    { name: "The_Wheel_of_Fortune", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Wheel_of_Fortune.jpg.jpg" },
-    { name: "Strength", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/Strength.jpg.jpg" },
-    { name: "The_Chariot", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Chariot.jpg.jpg" },
-    { name: "Justice", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/Justice.jpg.jpg" },
-    { name: "The_Hermit", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Hermit.jpg.jpg" },
-    { name: "The_High_Priestess", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_High_Priestess.jpg.jpg" },
-    { name: "The_Emperor", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Emperor.jpg.jpg" },
-    { name: "The_Sun", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Sun.jpg.jpg" },
-    { name: "The_Moon", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Moon.jpg.jpg" },
-    { name: "Temperance", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/Temperance.jpg.jpg" },
-    { name: "The_World", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_World.jpg.jpg" },
-    { name: "Judgement", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/Judgement.jpg.jpg" },
-    { name: "The_Devil", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Devil.jpg.jpg" },
-    { name: "The_Pope", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Pope.jpg.jpg" }
+    { name: "The Magician", image: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Magician.jpg.jpg" },
+    { name: "The High Priestess", image: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_High_Priestess.jpg.jpg" },
+    { name: "The Empress", image: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Empress.jpg.jpg" },
+    { name: "The Emperor", image: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Emperor.jpg.jpg" },
+    { name: "The Hierophant", image: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Hierophant.jpg.jpg" },
+    { name: "The Lovers", image: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Lovers.jpg.jpg" },
+    { name: "The Chariot", image: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Chariot.jpg.jpg" },
+    { name: "Strength", image: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/Strength.jpg.jpg" },
+    { name: "The Hermit", image: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Hermit.jpg.jpg" },
+    { name: "Wheel of Fortune", image: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/Wheel_of_Fortune.jpg.jpg" },
+    { name: "Justice", image: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/Justice.jpg.jpg" },
+    { name: "The Hanged Man", image: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Hanged_Man.jpg.jpg" },
+    { name: "Death", image: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/Death.jpg.jpg" },
+    { name: "Temperance", image: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/Temperance.jpg.jpg" },
+    { name: "The Devil", image: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Devil.jpg.jpg" },
+    { name: "The Tower", image: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Tower.jpg.jpg" },
+    { name: "The Star", image: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Star.jpg.jpg" },
+    { name: "The Moon", image: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Moon.jpg.jpg" },
+    { name: "The Sun", image: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Sun.jpg.jpg" },
+    { name: "Judgment", image: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/Judgment.jpg.jpg" },
+    { name: "The World", image: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_World.jpg.jpg" }
 ];
 
 function pickCards(num) {
-    const pickedCards = [];
-    for (let i = 0; i < num; i++) {
-        const randomIndex = Math.floor(Math.random() * cards.length);
-        pickedCards.push(cards[randomIndex]);
+    const picked = [];
+    while (picked.length < num) {
+        const randomCard = cards[Math.floor(Math.random() * cards.length)];
+        if (!picked.includes(randomCard)) {
+            picked.push(randomCard);
+        }
     }
 
-    displayCards(pickedCards);
-}
+    const pickedCardsDiv = document.getElementById("picked-cards");
+    pickedCardsDiv.innerHTML = ""; // Clear previous cards
 
-function displayCards(cardsToDisplay) {
-    const pickedCardsContainer = document.getElementById("picked-cards");
-    pickedCardsContainer.innerHTML = ""; // Clear any previously picked cards
-
-    cardsToDisplay.forEach(card => {
+    picked.forEach(card => {
         const imgElement = document.createElement("img");
-        imgElement.src = card.img;
+        imgElement.src = card.image;
         imgElement.alt = card.name;
-        imgElement.classList.add("card");
-        pickedCardsContainer.appendChild(imgElement);
+        imgElement.className = "card";
+        pickedCardsDiv.appendChild(imgElement);
     });
 }
