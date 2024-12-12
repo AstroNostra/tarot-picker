250bfd27b627cac18ded5bdcda5e911ad4e3fb24
AstroNostra <astronostra2023@gmail.com>
2024-12-12 12:50:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,47 @@
-// Define the card names as they appear in your GitHub repository
-const cardArray = [
-    "The_Fool.jpg.jpg", 
-    "The_Magician.jpg.jpg", 
-    "The_High_Priestess.jpg.jpg", 
-    "The_Empress.jpg.jpg", 
-    "The_Emperor.jpg.jpg", 
-    "The_Hierophant.jpg.jpg", 
-    "The_Lovers.jpg.jpg", 
-    "The_Chariot.jpg.jpg", 
-    "Strength.jpg.jpg", 
-    "The_Hermit.jpg.jpg", 
-    "Wheel_of_Fortune.jpg.jpg", 
-    "Justice.jpg.jpg", 
-    "The_Hanged_Man.jpg.jpg", 
-    "Death.jpg.jpg", 
-    "Temperance.jpg.jpg", 
-    "The_Devil.jpg.jpg", 
-    "The_Tower.jpg.jpg", 
-    "The_Star.jpg.jpg", 
-    "The_Moon.jpg.jpg", 
-    "The_Sun.jpg.jpg", 
-    "Judgement.jpg.jpg", 
-    "The_World.jpg.jpg"
+const cards = [
+    { name: "The_Magician", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Magician.jpg.jpg" },
+    { name: "The_Empress", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Empress.jpg.jpg" },
+    { name: "Death", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/Death.jpg.jpg" },
+    { name: "The_Fool", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Fool.jpg.jpg" },
+    { name: "The_Hanged_Man", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Hanged_Man.jpg.jpg" },
+    { name: "The_Lovers", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Lovers.jpg.jpg" },
+    { name: "The_Star", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Star.jpg.jpg" },
+    { name: "The_Tower", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Tower.jpg.jpg" },
+    { name: "The_Wheel_of_Fortune", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Wheel_of_Fortune.jpg.jpg" },
+    { name: "Strength", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/Strength.jpg.jpg" },
+    { name: "The_Chariot", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Chariot.jpg.jpg" },
+    { name: "Justice", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/Justice.jpg.jpg" },
+    { name: "The_Hermit", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Hermit.jpg.jpg" },
+    { name: "The_High_Priestess", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_High_Priestess.jpg.jpg" },
+    { name: "The_Emperor", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Emperor.jpg.jpg" },
+    { name: "The_Sun", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Sun.jpg.jpg" },
+    { name: "The_Moon", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Moon.jpg.jpg" },
+    { name: "Temperance", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/Temperance.jpg.jpg" },
+    { name: "The_World", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_World.jpg.jpg" },
+    { name: "Judgement", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/Judgement.jpg.jpg" },
+    { name: "The_Devil", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Devil.jpg.jpg" },
+    { name: "The_Pope", img: "https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/The_Pope.jpg.jpg" }
 ];
 
-// Function to pick a number of cards randomly
-function pickCards(numberOfCards) {
+function pickCards(num) {
     const pickedCards = [];
-    const cardsContainer = document.getElementById('cards');
-    cardsContainer.innerHTML = '';  // Clear previous cards
-
-    for (let i = 0; i < numberOfCards; i++) {
-        // Randomly select a card
-        const randomIndex = Math.floor(Math.random() * cardArray.length);
-        const selectedCard = cardArray[randomIndex];
-
-        // Create an image element
-        const cardElement = document.createElement('div');
-        cardElement.classList.add('card');
-        cardElement.style.backgroundImage = `url(https://raw.githubusercontent.com/astronostra/tarot-picker/main/images/${selectedCard})`;
-        
-        // Add the selected card to the container
-        cardsContainer.appendChild(cardElement);
+    for (let i = 0; i < num; i++) {
+        const randomIndex = Math.floor(Math.random() * cards.length);
+        pickedCards.push(cards[randomIndex]);
     }
+
+    displayCards(pickedCards);
+}
+
+function displayCards(cardsToDisplay) {
+    const pickedCardsContainer = document.getElementById("picked-cards");
+    pickedCardsContainer.innerHTML = ""; // Clear any previously picked cards
+
+    cardsToDisplay.forEach(card => {
+        const imgElement = document.createElement("img");
+        imgElement.src = card.img;
+        imgElement.alt = card.name;
+        imgElement.classList.add("card");
+        pickedCardsContainer.appendChild(imgElement);
+    });
 }
